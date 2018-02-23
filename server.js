a457883eaed8264ed92c41fa0c547e45e5a4fb3a
Plasmoxy <sebohacker@gmail.com>
2018-02-24 00:44:04 +0100

    maybe a working server

--- server.js
+++ server.js
@@ -1,17 +1,39 @@
 var express = require('express');
-var fs = require('fx');
-var https = require('https');
+//var httpsRedirect = require('express-https-redirect');
+var fs = require('fs');
 var http = require('http');
-var forceSsl = require('express-force-ssl');
+var https = require('https');
 
-var app = express();
-app.use(forceSsl);
-
-var options = {
+var ssl_options = {
   key: fs.readFileSync('ssl/private.key'),
-  cert: fs.readFileSync('ssl/primary.crt'),
+  cert: fs.readFileSync('ssl/certificate.crt'),
   ca: fs.readFileSync('ssl/ca_bundle.crt')
 };
 
-https.createServer(options, app).listen(443);
-http.createServer(app).listen(80);
+var app = express();
+
+var secureServer = https.createServer(ssl_options, app);
+var server = http.createServer(app);
+
+app.get('*', function (req, res, next) {
+  !req.secure ? res.redirect('https://plasmoxy.openode.io' + req.baseUrl) : next();
+})
+
+// ---
+
+var count = 0;
+
+app.get('/', function (req, res, next) {
+  res.send('xd');
+})
+
+app.get('/count', function (req, res, next)) {
+  count++;
+  res.send('count = ' + count);
+})
+
+
+// ---
+
+secureServer.listen(443)
+server.listen(80)
